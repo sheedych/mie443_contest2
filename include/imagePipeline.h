4b9e5e651f5d798db3171c4dd4b63af4a70545b9
Cameron Alizadeh <cdalizadeh@gmail.com>
2020-03-03 16:08:29 -0500

    Remove changes to imagePipeline

--- include/imagePipeline.h
+++ include/imagePipeline.h
@@ -6,11 +6,6 @@
 #include <cv.h>
 #include <cv_bridge/cv_bridge.h>
 #include <boxes.h>
-#include "opencv2/features2d.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/calib3d.hpp"
-#include "opencv2/xfeatures2d.hpp"
-#include "opencv2/imgproc.hpp"
 
 class ImagePipeline
 {
--- src/imagePipeline.cpp
+++ src/imagePipeline.cpp
@@ -1,9 +1,7 @@
 #include <imagePipeline.h>
 
 #define IMAGE_TYPE sensor_msgs::image_encodings::BGR8
-#define IMAGE_TOPIC "camera/image" // kinect:"camera/rgb/image_raw" webcam:"camera/image"
-using namespace cv;
-using namespace cv::xfeatures2d;
+#define IMAGE_TOPIC "camera/rgb/image_raw" // kinect:"camera/rgb/image_raw" webcam:"camera/image"
 
 ImagePipeline::ImagePipeline(ros::NodeHandle &n)
 {
@@ -49,87 +47,7 @@ int ImagePipeline::getTemplateID(Boxes &boxes)
     {
         /***YOUR CODE HERE***/
         // Use: boxes.templates
-
-        int minHessian = 400;
-        Mat img_object = boxes.templates.front();
-        Mat img_scene = img;
-
-        if (!img_object.data || !img_scene.data)
-            std::cout << "cant read" << std::endl;
-
-        // Steps 1 and 2, detect the keypoints and calculate descriptors uding SURF
-        // TODO: Change this to Lowe's thiny
-
-        Ptr<SURF> detector = SURF::create(minHessian);
-
-        std::vector<KeyPoint> keypoints_object, keypoints_scene;
-        Mat descriptors_object, descriptors_scene;
-
-        detector->detectAndCompute(img_object, Mat(), keypoints_object, descriptors_object);
-        detector->detectAndCompute(img_scene, Mat(), keypoints_scene, descriptors_scene);
-
-        FlannBasedMatcher matcher;
-        std::vector<DMatch> matches;
-        matcher.match(descriptors_object, descriptors_scene, matches);
-
-        double max_dist = 0;
-        double min_dist = 100;
-
-        for (int i = 0; i < descriptors_object.rows; i++)
-        {
-            double dist = matches[i].distance;
-            if (dist < min_dist)
-                min_dist = dist;
-            if (dist > max_dist)
-                max_dist = dist;
-        }
-
-        printf(" -- Max dist : %f \n", max_dist);
-        printf(" -- Min dist : %f \n", min_dist);
-
-        std::vector<DMatch> good_matches;
-
-        for (int i = 0; i < descriptors_object.rows; i++)
-        {
-            if (matches[i].distance < 3 * min_dist)
-            {
-                good_matches.push_back(matches[i]);
-            }
-        }
-
-        Mat img_matches;
-        drawMatches(img_object, keypoints_object, img_scene, keypoints_scene, good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-
-        std::vector<Point2f> obj;
-        std::vector<Point2f> scene;
-
-        for (int i = 0; i < good_matches.size(); i++)
-        {
-
-            obj.push_back(keypoints_object[good_matches[i].queryIdx].pt);
-            scene.push_back(keypoints_scene[good_matches[i].trainIdx].pt);
-        }
-
-        Mat H = findHomography(obj, scene, RANSAC);
-
-        //--Get the corners from the image_1 (the object to be "detected")
-        std::vector<Point2f> obj_corners(4);
-        obj_corners[0] = cvPoint(0, 0);
-        obj_corners[1] = cvPoint(img_object.cols, 0);
-        obj_corners[2] = cvPoint(img_object.cols, img_object.rows);
-        obj_corners[3] = cvPoint(0, img_object.rows);
-        std::vector<Point2f> scene_corners(4);
-
-        perspectiveTransform(obj_corners, scene_corners, H);
-
-        //-- Draw lines between the corners (the mapped object in the scene - image_2)
-        line(img_matches, scene_corners[0] + Point2f(img_object.cols, 0), scene_corners[1] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
-        line(img_matches, scene_corners[1] + Point2f(img_object.cols, 0), scene_corners[2] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
-        line(img_matches, scene_corners[2] + Point2f(img_object.cols, 0), scene_corners[3] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
-        line(img_matches, scene_corners[3] + Point2f(img_object.cols, 0), scene_corners[0] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
-
-        std::cout << "Displaying image now" << std::endl;
-        cv::imshow("view", img_matches);
+        cv::imshow("view", img);
         cv::waitKey(10);
     }
     return template_id;
